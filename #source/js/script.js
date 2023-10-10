ae166336b808a8ba1adc2f71eb5a5d288e25aa7d
Alex Melkov <alex.matvey93@gmail.com>
2023-10-10 16:12:34 +0700

    add adaptive section main-header

--- #source/js/script.js
+++ #source/js/script.js
@@ -3,10 +3,12 @@ window.addEventListener('load', function () {
   let header__burger = document.querySelector('.header-container__burger');
   let header_menu = document.querySelector('.header-container__menu');
   let lock = document.querySelector('body');
+  let mask = document.querySelector('.mask');
 
   header__burger.onclick = function () {
     header__burger.classList.toggle('active-menu');
     header_menu.classList.toggle('active-menu');
     lock.classList.toggle('lock');
+    mask.classList.toggle('mask-active');
   };
 });
